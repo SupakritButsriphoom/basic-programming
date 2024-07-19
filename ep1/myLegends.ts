e6e5b2693a2ef3b20c860f7aedb69355683d8633
SupakritButsriphoom <supakritboodstiphoom@gmail.com>
2024-07-19 11:42:53 +0700

    การบ้าน

---
+++ ep1/myLegends.ts
@@ -0,0 +1,11 @@
+const nickName: string = 'Got'
+const firstName: string = 'supakrit'
+
+let x: number = 1 //ตัวเลข
+let y: boolean = true //จริงเท็จ
+let z: string ='1' //ตัวอักษร
+let v: number = 2
+console.log(x===z) //เท่ากับ
+console.log(x<=v) //น้อยกว่าเท่ากับ
+console.log(x===v) //เท่ากับ
+console.log(x>v) //มากกว่า
