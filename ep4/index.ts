d1111e4f53c7df8473f5262b4e82d3d932050c57
superball <supakritboodstiphoom@gmail.com>
2024-08-02 11:46:25 +0700

    เรียน วันนี้

--- ep4/index.ts
+++ ep4/index.ts
@@ -55,3 +55,34 @@ function sayHi(dayOfweek: number) {
 }
 sayHi(12);
 */
+
+/* The code snippet is demonstrating some examples of number precision in JavaScript. */
+// const x: number = 999999999999999;
+// const y: number = 9999999999999999;
+// console.log(x);
+// console.log(y);
+
+// const z: number = 0.1 + 0.2;
+// console.log(z);
+
+// function sHa(n: number) {
+//   const x = n.toLocaleString();
+//   return x;
+// }
+// console.log(sHa(10000));
+
+// function fixedDecima(n: number, digit:number){
+//     const x= n.toFixed(digit)
+//     return x
+// }
+// console.log(fixedDecima(50.999,4))
+
+// function sHa(n:number){
+//     const x= n.toLocaleString()
+//     return x
+// }
+// console.log(sHa(10000))
+
+const str: string = "Supakrit";
+
+console.log(str.indexOf("r"));
