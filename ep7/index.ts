1d0fb1ee9cc781d38665eee1400a34bc8b282309
superball <supakritboodstiphoom@gmail.com>
2024-08-30 10:43:37 +0700

    เรียน อาทิตย์ Array 2.0

---
+++ ep7/index.ts
@@ -0,0 +1,25 @@
+// const studentsNameArray= studentsName.split(',')
+// console.log(studentsNameArray.join('*'))
+
+// const animalName:string='ตุ๊กแก*จิงจก*กิ้งก่า*ตัวเงินตัวทอง'
+// const animalNamearray=animalName.split('*')
+// console.log(animalNamearray)
+
+// const animalName:string='ตุ๊กแก*จิ้งจก*กิ้งก่า*ตัวเงินตัวทอง'
+// const animalNamearray:string[]=animalName.split('*')
+// console.log('เดิม: ',animalNamearray)
+// animalNamearray.pop()//ดึงตัวสุดท้ายออก
+// console.log('ดึงตัวสุดท้ายออก: ',animalNamearray)
+// animalNamearray.push('กาปิปาร่า')
+// console.log('ใส่ท้าย: ',animalNamearray)
+// animalNamearray.shift()
+// console.log('ดึงตัวหน้า: ',animalNamearray)
+// animalNamearray.unshift('ยีราฟ')
+// console.log('ใส่ตัวหน้า: ',animalNamearray)
+// console.log('เรียง ก-ฮ: ',animalNamearray.sort())
+// console.log('เรียง ฮ-ก: ',animalNamearray.reverse())
+// console.log('slice 0-2: ',animalNamearray.slice(0,2))
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.sort());
+console.log(fruits.reverse());
